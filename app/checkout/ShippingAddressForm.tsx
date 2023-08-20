5b6decf1830285c4a6fc9635ec7a3a7bee0ab8a9
grammatools1 <121976867+grammatools1@users.noreply.github.com>
2023-08-20 23:09:38 +0200

    Update ShippingAddressForm.tsx

--- app/checkout/ShippingAddressForm.tsx
+++ app/checkout/ShippingAddressForm.tsx
@@ -1,69 +1,67 @@
 'use client'
 
-import React, { forwardRef, useEffect, useRef, useState, useMemo } from 'react';
+import React, { useEffect, useRef, useState, useMemo } from 'react';
 import Medusa from "@medusajs/medusa-js";
-import { useCart, useRegions, useCartShippingOptions } from 'medusa-react';
-import { getNameList } from 'country-list';
+import { useCart, useCartShippingOptions } from 'medusa-react';
+import { getName, getNameList } from 'country-list';
+import Autocomplete from 'react-autocomplete';
+import ShippForm from './shippingform';
 
-type ShippingFormProps = {
-  onComplete: () => void;
-};
 const medusaBaseUrl = process.env.NEXT_PUBLIC_MEDUSA_BACKEND_API;
 const medusa = medusaBaseUrl ? new Medusa({ baseUrl: medusaBaseUrl, maxRetries: 3 }) : null;
 
-const ShippingForm = forwardRef(({ onComplete }: ShippingFormProps, ref) => {
+const useClickOutside = (ref, onClickOutside) => {
+  useEffect(() => {
+    const handleClick = (e) => {
+      if (ref.current && !ref.current.contains(e.target)) {
+        onClickOutside();
+      }
+    };
+    document.addEventListener('mousedown', handleClick);
+    return () => document.removeEventListener('mousedown', handleClick);
+  }, [ref, onClickOutside]);
+};
+
+const generateErrorMessage = (fieldName) => {
+  return `${fieldName.charAt(0).toUpperCase() + fieldName.slice(1)} is required`;
+};
+
+const ShippingForm = ({ onComplete }) => {
   const innerRef = useRef(null);
+  const [selectedCountry, setSelectedCountry] = useState(null);
   const [searchTerm, setSearchTerm] = useState('');
-  const [selectedOption, setSelectedOption] = useState('');
-  const [selectedShippingMethod, setSelectedShippingMethod] = useState('Standard');
   const [shippingOptions, setShippingOptions] = useState([]);
   const [shippingInfo, setShippingInfo] = useState({
-    company: '',
     first_name: '',
     last_name: '',
+    email: '',
     address_1: '',
     address_2: '',
     city: '',
-    country_code: '',
     province: '',
     postal_code: '',
     phone: '',
-    email: '',
+    country_code: '',
+    company: '',
   });
   const [validationErrors, setValidationErrors] = useState({});
-
-  const { regions } = useRegions();
   const { cart } = useCart();
   const { shippingMethods } = useCartShippingOptions();
 
-  const validateEmail = (email) => {
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-     }; 
-     const isPostalCodeRequired = (countryCode) => {
-        // Replace this logic with your actual logic to determine if postal code is required
-        const countriesRequiringPostalCode = ['US', 'CA', 'GB']; // Example countries
-        return countriesRequiringPostalCode.includes(countryCode);
-      };
-
   const countryOptions = useMemo(() => {
-    const currentRegion = regions?.find((r) => r.id === cart?.region_id);
-
-    if (!currentRegion) {
-      return [];
-    }
-
-    return currentRegion.countries.map((country) => ({
-      value: country.iso_2,
-      label: country.display_name,
+    const countryList = getNameList();
+    return countryList.map((countryCode) => ({
+      value: countryCode,
+      label: getName(countryCode),
     }));
-  }, [regions, cart]);
+  }, []);
+
+  const [selectedShippingOption, setSelectedShippingOption] = useState(null);
+  const [selectedShippingMethod, setSelectedShippingMethod] = useState('Standard');
 
   useEffect(() => {
-    const handleClickOutside = (e) => {
-      if (!innerRef.current.contains(e.target)) {
-        setSearchTerm('');
-      }
+    const handleClickOutside = () => {
+      setSearchTerm('');
     };
 
     document.addEventListener('mousedown', handleClickOutside);
@@ -71,8 +69,6 @@ const ShippingForm = forwardRef(({ onComplete }: ShippingFormProps, ref) => {
       document.removeEventListener('mousedown', handleClickOutside);
     };
   }, []);
-
-  // ... (validateEmail, handleSelectOption, handleShippingOptionChange, handleInputChange)
 
   useEffect(() => {
     if (medusa) {
@@ -85,81 +81,104 @@ const ShippingForm = forwardRef(({ onComplete }: ShippingFormProps, ref) => {
           console.error('Error fetching shipping options:', error);
         });
     }
-  }, [cart]);
+  }, [cart?.id]);
 
-  const handleSubmit = () => {
+  const validateEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const isPostalCodeRequired = (countryCode) => {
+    const countriesRequiringPostalCode = ['US', 'CA', 'GB'];
+    return countriesRequiringPostalCode.includes(countryCode);
+  };
+
+  const handleCountrySelect = (country) => {
+    setSelectedCountry(country);
+    setSearchTerm(country.label);
+    setShippingInfo((prevShippingInfo) => ({
+      ...prevShippingInfo,
+      country_code: country.value,
+    }));
+  };
+
+  const handleCountryChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setShippingInfo((prevShippingInfo) => ({
+      ...prevShippingInfo,
+      [name]: value,
+    }));
+  };
+
+  const handleShippingOptionChange = (e) => {
+    const selectedOptionId = e.target.value;
+    setSelectedShippingOption(selectedOptionId);
+  };
+
+  const handleSubmit = async () => {
     const errors = {};
 
-    if (!selectedOption) {
-      errors.country = 'Country is required';
+    if (!selectedCountry) {
+      errors.country = generateErrorMessage('country');
     }
 
-      // Validate required fields
-      if (!shippingInfo.first_name.trim()) {
-        errors.first_name = 'First Name is required';
-      }
-      if (!shippingInfo.last_name.trim()) {
-        errors.last_name = 'Last Name is required';
-      }
-      if (!shippingInfo.address_1.trim()) {
-        errors.address_1 = 'Address is required';
-      }
-      if (!shippingInfo.city.trim()) {
-        errors.city = 'City is required';
-      }
-      if (!shippingInfo.country_code) {
-        errors.country_code = 'Country is required';
-      }
-      if (!/^\+(?:[0-9] ?){6,14}[0-9]$/.test(shippingInfo.phone)) {
-        errors.phone = 'Invalid phone number';
-      }
-  
-      // Validate postal code (optional)
-      if (
-        shippingInfo.postal_code.trim() &&
-        !/^[0-9]{5}(?:-[0-9]{4})?$/.test(shippingInfo.postal_code)
-      ) {
-        errors.postal_code = 'Invalid Postal Code';
-      }
-  
-      // Validate company (optional)
-      if (shippingInfo.company.trim() && shippingInfo.company.length < 3) {
-        errors.company = 'Company name must be at least 3 characters';
-      }
-  
-      if (!validateEmail(shippingInfo.email)) {
-        errors.email = 'Invalid email address';
-      }
-      
+    if (!shippingInfo.first_name.trim()) {
+      errors.first_name = generateErrorMessage('first name');
+    }
+    if (!shippingInfo.last_name.trim()) {
+      errors.last_name = generateErrorMessage('last name');
+    }
+    if (!shippingInfo.address_1.trim()) {
+      errors.address_1 = generateErrorMessage('address');
+    }
+    if (!shippingInfo.city.trim()) {
+      errors.city = generateErrorMessage('city');
+    }
+    if (!shippingInfo.country_code) {
+      errors.country_code = generateErrorMessage('country');
+    }
+    if (!/^\+(?:[0-9] ?){6,14}[0-9]$/.test(shippingInfo.phone)) {
+      errors.phone = 'Invalid phone number';
+    }
 
+    if (
+      shippingInfo.postal_code.trim() &&
+      !/^[0-9]{5}(?:-[0-9]{4})?$/.test(shippingInfo.postal_code)
+    ) {
+      errors.postal_code = 'Invalid Postal Code';
+    }
+
+    if (shippingInfo.company.trim() && shippingInfo.company.length < 3) {
+      errors.company = 'Company name must be at least 3 characters';
+    }
+
+    if (!validateEmail(shippingInfo.email)) {
+      errors.email = 'Invalid email address';
+    }
     setValidationErrors(errors);
 
     if (Object.keys(errors).length === 0 && medusa) {
-      medusa.carts
-        .update(cart?.id, {
+      try {
+        await medusa.carts.update(cart?.id, {
           shipping_address: { ...shippingInfo },
           shipping_method: selectedShippingMethod,
-        })
-        .then(() => {
-          if (selectedShippingMethod === 'Local Pickup') {
-            onComplete();
-          } else {
-            medusa.carts
-              .addShippingMethod(cart?.id, {
-                option_id: selectedShippingOption,
-              })
-              .then(({ cart }) => {
-                console.log('Updated Shipping Methods:', cart.shipping_methods);
-                onComplete();
-              })
-              .catch((error) => {
-                console.error('Error adding shipping method:', error);
-              });
-          }
-        })
-        .catch((error) => {
-          console.error('Error updating shipping address:', error);
         });
+
+        if (selectedShippingMethod === 'Local Pickup') {
+          onComplete();
+        } else {
+          await medusa.carts.addShippingMethod(cart?.id, {
+            option_id: selectedShippingOption,
+          });
+          onComplete();
+        }
+      } catch (error) {
+        console.error('Error updating shipping address:', error);
+      }
     }
   };
 
@@ -167,168 +186,11 @@ const ShippingForm = forwardRef(({ onComplete }: ShippingFormProps, ref) => {
     <div>
       <h2>Shipping Information</h2>
       <form>
+        <ShippForm />
         <div>
-          <label htmlFor="country">Country:</label>
-          <input
-            type="text"
-            name="country"
-            placeholder="Country"
-            value={searchTerm}
-            onChange={handleInputChange}
-          />
-          {validationErrors.country && (
-            <span style={{ color: 'red' }}>{validationErrors.country}</span>
-          )}
-             <ul>
-            {countryOptions.map(({ value, label }, index) => (
-              <li
-                key={index}
-                onClick={() => handleSelectOption(value)}
-                onKeyDown={(e) => {
-                  if (e.key === 'Enter') {
-                    handleSelectOption(value);
-                  }
-                }}
-                role="option"
-                aria-selected={value === selectedOption}
-              >
-                {label}
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div>
-        <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            name="email"
-            placeholder="Email"
-            value={shippingInfo.email}
-            onChange={handleInputChange}
-          />
-          {validationErrors.email && (
-            <span style={{ color: 'red' }}>{validationErrors.email}</span>
-          )}
-        </div>
-        <div>
-        <label htmlFor="first_name">First Name:</label>
-          <input
-            type="text"
-            name="first_name"
-            placeholder="First Name"
-            value={shippingInfo.first_name}
-            onChange={handleInputChange}
-          />
-          {validationErrors.first_name && (
-            <span style={{ color: 'red' }}>{validationErrors.first_name}</span>
-          )}
-        </div>
-        <div>
-        <label htmlFor="last_name">Last Name:</label>
-          <input
-            type="text"
-            name="last_name"
-            placeholder="Last Name"
-            value={shippingInfo.last_name}
-            onChange={handleInputChange}
-          />
-          {validationErrors.last_name && (
-            <span style={{ color: 'red' }}>{validationErrors.last_name}</span>
-          )}
-        </div>
-        <div>
-        <label htmlFor="company">Company:</label>
-          <input
-            type="text"
-            name="company"
-            placeholder="Company (optional)"
-            value={shippingInfo.company}
-            onChange={handleInputChange}
-          />
-          {validationErrors.company && (
-            <span style={{ color: 'red' }}>{validationErrors.company}</span>
-          )}
-        </div>
-        <div>
-        <label htmlFor="address_1">Address 1:</label>
-          <input
-            type="text"
-            name="address_1"
-            placeholder="Address 1"
-            value={shippingInfo.address_1}
-            onChange={handleInputChange}
-          />
-          {validationErrors.address_1 && (
-            <span style={{ color: 'red' }}>{validationErrors.address_1}</span>
-          )}
-        </div>
-        <div>
-        <label htmlFor="address_2">Address_2:</label>
-          <input
-            type="text"
-            name="address_2"
-            placeholder="Address 2 (optional)"
-            value={shippingInfo.address_2}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div>
-        <label htmlFor="city">City:</label>
-          <input
-            type="text"
-            name="city"
-            placeholder="City"
-            value={shippingInfo.city}
-            onChange={handleInputChange}
-          />
-          {validationErrors.city && (
-            <span style={{ color: 'red' }}>{validationErrors.city}</span>
-          )}
-        </div>
-        
-        <div>
-        <label htmlFor="province">Province/State:</label>
-          <input
-            type="text"
-            name="province"
-            placeholder="State/Province"
-            value={shippingInfo.province}
-            onChange={handleInputChange}
-          />
-        </div>
-        {isPostalCodeRequired(shippingInfo.country_code) && (
-            <div>
-                 <label htmlFor="postal_code">Postal Code:</label>
-                <input
-                type="text"
-                name="postal_code"
-                placeholder="Postal Code"
-                value={shippingInfo.postal_code}
-                onChange={handleInputChange}
-                />
-                {validationErrors.postal_code && (
-                <span style={{ color: 'red' }}>{validationErrors.postal_code}</span>
-                )}
-            </div>
-            )}
-
-        <div>
-        <label htmlFor="phone">Phone:</label>
-          <input
-            type="text"
-            name="phone"
-            placeholder="Phone"
-            value={shippingInfo.phone}
-            onChange={handleInputChange}
-          />
-          {validationErrors.phone && (
-            <span style={{ color: 'red' }}>{validationErrors.phone}</span>
-          )}
-        </div>
-        <div>
-        <label>Shipping Methods:</label>
+          <label>Shipping Methods:</label>
           <select
-            value={selectedShippingMethod}
+            value={selectedShippingOption}
             onChange={handleShippingOptionChange}
           >
             {shippingOptions.map((option) => (
@@ -339,7 +201,7 @@ const ShippingForm = forwardRef(({ onComplete }: ShippingFormProps, ref) => {
           </select>
         </div>
         <div>
-        <label htmlFor="local_pickup">
+          <label htmlFor="local_pickup">
             <input
               type="checkbox"
               checked={selectedShippingMethod === 'Local Pickup'}
@@ -358,7 +220,6 @@ const ShippingForm = forwardRef(({ onComplete }: ShippingFormProps, ref) => {
       </form>
     </div>
   );
-});
+};
 
 export default ShippingForm;
-
