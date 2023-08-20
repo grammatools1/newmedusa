9dc795f8cbd86f9c691e7ae62b313531b96c1b2e
grammatools1 <121976867+grammatools1@users.noreply.github.com>
2023-08-20 09:20:44 +0200

    Update page.tsx

--- app/[page]/page.tsx
+++ app/[page]/page.tsx
@@ -1,13 +1,15 @@
+// Import necessary modules
 import type { Metadata } from 'next';
 import Prose from 'components/prose';
 import { CHECKOUT_PAGE_PROPS } from 'lib/constants';
 import { notFound } from 'next/navigation';
-import Checkout from 'components/checkout'; // Import the Checkout component
+import Checkout from 'components/checkout';
 
+// Set the runtime and revalidate settings
 export const runtime = 'edge';
-
 export const revalidate = 43200; // 12 hours
 
+// Function to generate metadata for the page
 export async function generateMetadata({
   params
 }: {
@@ -32,6 +34,7 @@ export async function generateMetadata({
   };
 }
 
+// Main page component
 export default function Page({ params }: { params: { page: string } }) {
   let page;
 
@@ -43,8 +46,11 @@ export default function Page({ params }: { params: { page: string } }) {
 
   return (
     <>
+      {/* Render the page title */}
       <h1 className="mb-8 text-5xl font-bold">{page.title}</h1>
+      {/* Render the page content using the Prose component */}
       <Prose className="mb-8" html={page.body as string} />
+      {/* Render the last updated date */}
       <p className="text-sm italic">
         {`This document was last updated on ${new Intl.DateTimeFormat(undefined, {
           year: 'numeric',
@@ -52,7 +58,8 @@ export default function Page({ params }: { params: { page: string } }) {
           day: 'numeric'
         }).format(new Date(page.updatedAt))}.`}
       </p>
-   
+      {/* Render the Checkout component if the page is 'checkout' */}
+      {params.page === 'checkout' && <Checkout />}
     </>
   );
 }
