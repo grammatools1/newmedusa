8ddfb317454292cbd0969606fb8e64c36bf78a02
grammatools1 <121976867+grammatools1@users.noreply.github.com>
2023-08-21 21:40:22 +0200

    Update ShippingAddressForm.tsx

--- app/checkout/ShippingAddressForm.tsx
+++ app/checkout/ShippingAddressForm.tsx
@@ -212,7 +212,7 @@ const handleClearCountry = () => {
             value={shippingInfo.first_name}
             onChange={handleInputChange}
           />
-         {validationErrors && validationErrors.first_name !== undefined && (
+         {validationErrors && validationErrors.hasOwnProperty('first_name') && (
             <span style={{ color: 'red' }}>{validationErrors.first_name}</span>
           )}
         </div>
