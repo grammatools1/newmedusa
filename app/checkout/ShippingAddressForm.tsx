81180713f499e3ff5d6d864152d570d7899d27ef
grammatools1 <121976867+grammatools1@users.noreply.github.com>
2023-08-25 21:00:08 +0200

    Update ShippingAddressForm.tsx

--- app/checkout/ShippingAddressForm.tsx
+++ app/checkout/ShippingAddressForm.tsx
@@ -2,7 +2,7 @@
 
 import React, { useEffect, useState } from 'react';
 import { useForm, Controller } from 'react-hook-form';
-import Medusa, { ValidationError } from '@medusajs/medusa-js';
+import Medusa from '@medusajs/medusa-js';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 import ShippingFormFields from './ShippingFormFields';
