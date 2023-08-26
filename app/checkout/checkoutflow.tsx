9a261fe8ca81d06a0af81df58ed0187db240dd0b
grammatools1 <121976867+grammatools1@users.noreply.github.com>
2023-08-26 12:27:12 +0200

    Update checkoutflow.tsx

--- app/checkout/checkoutflow.tsx
+++ app/checkout/checkoutflow.tsx
@@ -1,61 +1,65 @@
-import React from 'react';
-import ShippingForm from './ShippingAddressForm'; // Import the ShippingForm component (provide the correct path)
+import React, { useState, useEffect } from 'react';
 import Medusa from "@medusajs/medusa-js";
+import ShippingForm from './ShippingAddressForm'; // Import the ShippingForm component
 
-const Checkoutflow = () => {
-  const MEDUSA_BACKEND_API = 'process.env.MEDUSA_API_KEY'; // Replace with your Medusa backend API URL
-  const medusa = new Medusa({ baseUrl:process.env.NEXT_PUBLIC_MEDUSA_BACKEND_API, maxRetries: 3 });
+const CheckoutFlow = () => {
+  const MEDUSA_BACKEND_API = process.env.MEDUSA_API_KEY; // Replace with your Medusa backend API URL
+  const medusa = new Medusa({ baseUrl: MEDUSA_BACKEND_API, maxRetries: 3 });
 
-  // State to store cartId
   const cartId = localStorage.getItem("cart_id");
 
-  // State hooks used in multiple parts of the component
-  const [cartItems, setCartItems] = React.useState([]); // Store cart items
-  const [selectedPaymentMethod, setSelectedPaymentMethod] = React.useState(''); // Selected payment method
-  const [couponCode, setCouponCode] = React.useState(''); // Coupon code input
-  const [giftCardCode, setGiftCardCode] = React.useState(''); // Gift card code input
-  const [orderTotal, setOrderTotal] = React.useState(0); // Order total amount
-  const [step, setStep] = React.useState(1); // Current step of the checkout process
+  const [cartItems, setCartItems] = useState([]);
+  const [selectedPaymentMethod, setSelectedPaymentMethod] = useState('');
+  const [couponCode, setCouponCode] = useState('');
+  const [giftCardCode, setGiftCardCode] = useState('');
+  const [orderTotal, setOrderTotal] = useState(0);
+  const [step, setStep] = useState(1);
 
-  // Function to proceed to the next shipping step
+  useEffect(() => {
+    // Fetch cart items
+    if (cartId) {
+      medusa.carts.retrieve(cartId)
+        .then(({ cart }) => {
+          setCartItems(cart.items);
+          setOrderTotal(cart.total);
+        })
+        .catch((error) => {
+          console.error('Error fetching cart:', error);
+        });
+    }
+  }, [cartId]);
+
   const handleShippingComplete = () => {
     setStep(2);
   };
 
-  // Function to proceed to the next payment step
   const handlePaymentComplete = () => {
     setStep(3);
   };
 
-  // Function to handle placing the order
   const handlePlaceOrder = () => {
-    // Final step: Complete the checkout process
     const paymentData = {
       provider_id: selectedPaymentMethod,
       data: {
-        // Add any required payment data for the selected method (e.g., crypto wallet address)
+        // Add any required payment data for the selected method
       },
     };
 
-    medusa.carts
-      .setPaymentSession(cartId, paymentData)
+    medusa.carts.setPaymentSession(cartId, paymentData)
       .then(() => {
         return medusa.carts.complete(cartId);
       })
       .then(({ type, data }) => {
         console.log('Checkout Completed:', type, data);
-        // Display order confirmation or handle any further actions
+        // Display order confirmation or handle further actions
       })
       .catch((error) => {
         console.error('Error completing checkout:', error);
       });
   };
 
-  // Function to apply a coupon code
   const handleApplyCoupon = () => {
-    // Apply the coupon code and update the order total
-    medusa.carts
-      .applyCoupon(cartId, couponCode)
+    medusa.carts.applyCoupon(cartId, couponCode)
       .then(({ cart }) => {
         setOrderTotal(cart.total);
       })
@@ -64,11 +68,8 @@ const Checkoutflow = () => {
       });
   };
 
-  // Function to apply a gift card code
   const handleApplyGiftCard = () => {
-    // Apply the gift card code and update the order total
-    medusa.carts
-      .applyGiftCard(cartId, giftCardCode)
+    medusa.carts.applyGiftCard(cartId, giftCardCode)
       .then(({ cart }) => {
         setOrderTotal(cart.total);
       })
@@ -77,42 +78,38 @@ const Checkoutflow = () => {
       });
   };
 
-  // JSX rendering based on the current step
   return (
     <div>
       {step === 1 && (
         <div>
-          {/* Step 1: Cart Review */}
           <h1>Step 1: Cart Review</h1>
+          {/* Display cart items */}
+          {cartItems.map((item) => (
+            <div key={item.id}>
+              <p>Product: {item.product.title}</p>
+              <p>Quantity: {item.quantity}</p>
+              <p>Total Price: ${item.total}</p>
+            </div>
+          ))}
           <div>
-            {/* Display your cart items here */}
-            {cartItems.map((item) => (
-              <div key={item.id}>
-                <p>Product: {item.product.title}</p>
-                <p>Quantity: {item.quantity}</p>
-                <p>Total Price: ${item.total}</p>
-              </div>
-            ))}
-            <div>
-              <label>Coupon Code:</label>
-              <input
-                type="text"
-                value={couponCode}
-                onChange={(e) => setCouponCode(e.target.value)}
-              />
-              <button onClick={handleApplyCoupon}>Apply Coupon</button>
-            </div>
-            <div>
-              <label>Gift Card Code:</label>
-              <input
-                type="text"
-                value={giftCardCode}
-                onChange={(e) => setGiftCardCode(e.target.value)}
-              />
-              <button onClick={handleApplyGiftCard}>Apply Gift Card</button>
-            </div>
-            <p>Order Total: ${orderTotal}</p>
+            <label>Coupon Code:</label>
+            <input
+              type="text"
+              value={couponCode}
+              onChange={(e) => setCouponCode(e.target.value)}
+            />
+            <button onClick={handleApplyCoupon}>Apply Coupon</button>
           </div>
+          <div>
+            <label>Gift Card Code:</label>
+            <input
+              type="text"
+              value={giftCardCode}
+              onChange={(e) => setGiftCardCode(e.target.value)}
+            />
+            <button onClick={handleApplyGiftCard}>Apply Gift Card</button>
+          </div>
+          <p>Order Total: ${orderTotal}</p>
           <button onClick={handleShippingComplete}>Proceed to Shipping</button>
         </div>
       )}
@@ -121,54 +118,27 @@ const Checkoutflow = () => {
       )}
       {step === 3 && (
         <div>
-          {/* Step 3: Payment Method Selection */}
           <h1>Step 3: Payment Method Selection</h1>
-          <div>
-            {/* Display payment method options */}
-            <label>
-              <input
-                type="radio"
-                name="paymentMethod"
-                value="credit_card"
-                checked={selectedPaymentMethod === 'credit_card'}
-                onChange={() => setSelectedPaymentMethod('credit_card')}
-              />
-              Credit Card
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="paymentMethod"
-                value="paypal"
-                checked={selectedPaymentMethod === 'paypal'}
-                onChange={() => setSelectedPaymentMethod('paypal')}
-              />
-              PayPal
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="paymentMethod"
-                value="crypto"
-                checked={selectedPaymentMethod === 'crypto'}
-                onChange={() => setSelectedPaymentMethod('crypto')}
-              />
-              Cryptocurrency
-            </label>
-            {/* Add more payment method options */}
-          </div>
+          {/* Display payment method options */}
+          <label>
+            <input
+              type="radio"
+              name="paymentMethod"
+              value="credit_card"
+              checked={selectedPaymentMethod === 'credit_card'}
+              onChange={() => setSelectedPaymentMethod('credit_card')}
+            />
+            Credit Card
+          </label>
+          {/* Add more payment method options */}
           <button onClick={handlePaymentComplete}>Proceed to Order Review</button>
         </div>
       )}
-       {step === 4 && (
+      {step === 4 && (
         <div>
-          {/* Step 4: Order Review */}
           <h1>Step 4: Order Review</h1>
-          <div>
-            {/* Display order summary */}
-            <p>Selected Payment Method: {selectedPaymentMethod}</p>
-            {/* Display other order details like shipping method, total price, etc. */}
-          </div>
+          <p>Selected Payment Method: {selectedPaymentMethod}</p>
+          {/* Display other order details */}
           <button onClick={handlePlaceOrder}>Place Order</button>
         </div>
       )}
@@ -176,4 +146,4 @@ const Checkoutflow = () => {
   );
 };
 
-export default Checkoutflow;
+export default CheckoutFlow;
