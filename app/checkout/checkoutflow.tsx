7175731dcc3f072f5eca74e0f9bb06b7149d83b3
grammatools1 <121976867+grammatools1@users.noreply.github.com>
2023-08-26 16:07:50 +0200

    Update checkoutflow.tsx

--- app/checkout/checkoutflow.tsx
+++ app/checkout/checkoutflow.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React, { useState, useEffect } from 'react';
 import Medusa from "@medusajs/medusa-js";
 import ShippingForm from './ShippingAddressForm'; // Import the ShippingForm component
