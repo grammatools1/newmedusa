ae951adbaecf1e4b6a2d30370e022039ff0a5497
grammatools1 <121976867+grammatools1@users.noreply.github.com>
2023-08-27 08:47:12 +0200

    Update checkoutflow.tsx

--- app/checkout/checkoutflow.tsx
+++ app/checkout/checkoutflow.tsx
@@ -43,7 +43,7 @@ const CheckoutFlow = () => {
     initializeMedusa();
   }, []);
   
- const cartId = localStorage.getItem("cart_id");
+ const cartId = "cart_01H88SMQFPQCRY7TVFW56GJVDB";
   
   const fetchCartItems = () => {
     if (medusa && cartId) {
