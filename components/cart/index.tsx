58ae0513c63fc17de6acbe89eeaf9a1a3e4e8368
grammatools1 <121976867+grammatools1@users.noreply.github.com>
2023-08-27 12:17:11 +0200

    Update index.tsx

--- components/cart/index.tsx
+++ components/cart/index.tsx
@@ -1,8 +1,9 @@
 import { createCart, getCart } from 'lib/medusa';
 import { cookies } from 'next/headers';
 import CartModal from './modal';
+import CheckoutFlow from './CheckoutFlow';
 
-export default async function Cart() {
+export default function Cart() {
   const cartId = cookies().get('cartId')?.value;
   let cart;
 
@@ -12,10 +13,17 @@ export default async function Cart() {
 
   // If the `cartId` from the cookie is not set or the cart is empty
   // (old carts becomes `null` when you checkout), then get a new `cartId`
-  //  and re-fetch the cart.
+  // and re-fetch the cart.
   if (!cartId || !cart) {
     cart = await createCart();
+    document.cookie = `cartId=${cart.id}; path=/`;
+    localStorage.setItem('cart_id', cart.id);
   }
 
-  return <CartModal cart={cart} />;
+  return (
+    <>
+      <CartModal cart={cart} />
+      <CheckoutFlow cart={cart} />
+    </>
+  );
 }
