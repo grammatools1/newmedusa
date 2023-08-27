7cebb6d08348e35ebf99dc96462feedd59f30cc0
grammatools1 <121976867+grammatools1@users.noreply.github.com>
2023-08-27 14:56:33 +0200

    Update ShippingAddressForm.tsx

--- app/checkout/ShippingAddressForm.tsx
+++ app/checkout/ShippingAddressForm.tsx
@@ -1,35 +1,17 @@
 "use client"
 
-import React, { useEffect, useState } from 'react';
-import { useForm, Controller, SubmitHandler, Control} from 'react-hook-form';
+import React, { useState, useEffect } from 'react';
+import { Controller, SubmitHandler, useForm, useWatch } from 'react-hook-form';
 import Medusa from '@medusajs/medusa-js';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
-import ShippingFormFields from './ShippingFormFields';
 import { ValidationError } from 'yup';
-const countryListModule = require('country-list');
+import ShippingFormFields from './ShippingFormFields';
 
-const medusaBaseUrl = process.env.NEXT_PUBLIC_MEDUSA_BACKEND_API;
-const medusa = medusaBaseUrl ? new Medusa({ baseUrl: medusaBaseUrl, maxRetries: 3 }) : null;
-
-const validationSchema = yup.object().shape({
-   firstName: yup.string().required('First Name is required'),
-  lastName: yup.string().required('Last Name is required'),
-  email: yup.string().email('Invalid email address').required('Email is required'),
-  address1: yup.string().required('Address is required'),
-  city: yup.string().required('City is required'),
-  countryCode: yup.string().required('Country is required'),
-  postalCode: yup
-    .string()
-    .matches(/^\d{5}(-\d{4})?$/, 'Invalid postal code')
-    .required('Postal code is required'),
-  phone: yup
-    .string()
-    .matches(/^\+(?:[0-9] ?){6,14}[0-9]$/, 'Invalid phone number')
-    .required('Phone is required'),
-  company: yup.string().min(3, 'Company name must be at least 3 characters long')
-});
-
+interface Props {
+  cart: any; // Replace 'any' with your actual cart type
+  onComplete: () => void;
+}
 
 interface CombinedFormData {
   firstName: string;
@@ -45,156 +27,246 @@ interface CombinedFormData {
   acceptUpdates?: boolean;
 }
 
-type SubmitType = SubmitHandler<CombinedFormData>;
+const validationSchema = yup.object().shape({
+  firstName: yup.string().required('First Name is required'),
+  lastName: yup.string().required('Last Name is required'),
+  email: yup.string().email('Invalid email address').required('Email is required'),
+  address1: yup.string().required('Address is required'),
+  city: yup.string().required('City is required'),
+  countryCode: yup.string().required('Country is required'),
+  postalCode: yup
+    .string()
+    .matches(/^\d{5}(-\d{4})?$/, 'Invalid postal code')
+    .required('Postal code is required'),
+  phone: yup
+    .string()
+    .matches(/^\+(?:[0-9] ?){6,14}[0-9]$/, 'Invalid phone number')
+    .required('Phone is required'),
+  company: yup.string().min(3, 'Company name must be at least 3 characters long'),
+});
 
-function ShippingForm(props: Props) = ({ onComplete }: { onComplete: () => void }) => {
-  const { cart } = props;
-  const { control, handleSubmit, formState } = useForm({
+const medusaBaseUrl = process.env.NEXT_PUBLIC_MEDUSA_BACKEND_API;
+const medusa = medusaBaseUrl ? new Medusa({ baseUrl: medusaBaseUrl, maxRetries: 3 }) : null;
+
+const ShippingForm = ({ cart, onComplete }: Props) => {
+  const [selectedShippingMethod, setSelectedShippingMethod] = useState('');
+  const [shippingOptions, setShippingOptions] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<Error>();
+  const [acceptUpdates, setAcceptUpdates] = useState(false);
+
+  const { control, handleSubmit, formState } = useForm<CombinedFormData>({
     resolver: yupResolver(validationSchema),
   });
 
   const { errors } = formState;
-  const [cart, setCart] = useState<any>(null); // Replace 'any' with your actual cart type
-  const [selectedShippingMethod, setSelectedShippingMethod] = useState('');
-  const [shippingOptions, setShippingOptions] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState('');
-  const [acceptUpdates, setAcceptUpdates] = useState(false);
 
-
-   const countryOptions = React.useMemo(() => {
-    const countries = countryListModule.getNameList();
+  const countryOptions = React.useMemo(() => {
+    const countries = require('country-list').getNameList();
     return countries.map((countryCode: string) => ({
       value: countryCode,
-      label: countryListModule.getName(countryCode),
+      label: require('country-list').getName(countryCode),
     }));
   }, []);
 
+  useEffect(() => {
+    if (!cart) {
+      return;
+    }
 
-    useEffect(() => {
-    fetchCartItems(cart);
+    const fetchCartItems = async () => {
+      if (!medusa) {
+        console.error('Medusa not initialized');
+        return;
+      }
+
+      try {
+        setIsLoading(true);
+        const { cart: updatedCart } = await medusa.carts.retrieve(cart.id);
+        // Replace below `console.log` statements with your own custom logic
+        console.log(`Total: ${updatedCart.total}`);
+        console.log(`Items: ${JSON.stringify(updatedCart.items)}`);
+      } catch (error) {
+        console.error('Error fetching cart items:', error);
+        // Replace below `toast` statement with your own custom logic
+        console.error('Failed to fetch cart items. Please refresh the page.');
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchCartItems();
   }, [cart]);
 
-  const fetchCartItems = async (cart: { id: string }) => {
-  if (!medusa) {
-    console.error('Medusa not initialized');
-    return;
-  }
-
-  try {
-    setLoading(true);
-    const { cart: updatedCart } = await medusa.carts.retrieve(cart.id);
-    setOrderTotal(updatedCart.total);
-    setCartItems(updatedCart.items);
-  } catch (error) {
-    console.error('Error fetching cart items:', error);
-    toast.error('Failed to fetch cart items. Please refresh the page.', { autoClose: 3000 });
-  } finally {
-    setLoading(false);
-  }
-  };
-
-   useEffect(() => {
+  useEffect(() => {
     const fetchShippingOptions = async () => {
       try {
-        if (medusa) {
-          const { shipping_options } = await medusa.shippingOptions.list();
-          setShippingOptions(shipping_options);
+        if (!medusa) {
+          console.error('Medusa not initialized');
+          return
         }
+
+        const { shipping_options } = await medusa.shippingOptions.list();
+        setShippingOptions(shipping_options);
       } catch (error) {
         console.error('Error retrieving shipping options', error);
-        setError('Error retrieving shipping options');
+        setError(error);
       }
     };
 
     fetchShippingOptions();
   }, []);
-   
-  const handleFormSubmit: SubmitType = async (data) => {
-  const {
-    firstName,
-    lastName,
-    email,
-    address1,
-    city,
-    province,
-    countryCode,
-    postalCode,
-    phone,
-    company,
-    acceptUpdates,
-  } = data;
+
+  const handleFormSubmit: SubmitHandler<CombinedFormData> = async (data) => {
+    const {
+      firstName,
+      lastName,
+      email,
+      address1,
+      city,
+      province,
+      countryCode,
+      postalCode,
+      phone,
+      company,
+      acceptUpdates,
+    } = data;
+
     try {
       setIsLoading(true);
-      setError('');
+      setError(undefined);
 
       if (medusa && cart && cart.id) {
         const cartId = cart.id as string; // Type assertion
 
         // Update shipping address and method
         await medusa.carts.update(cartId, {
-        shipping_address: {
-            company: data.company,
-            first_name: data.firstName,
-            last_name: data.lastName,
-            email: data.email,
-            address_1: data.address1,
-            city: data.city,
-            province: data.province,
-            postal_code: data.postalCode,
-            country_code: data.countryCode,
-            phone: data.phone,
-            acceptUpdates: data.acceptUpdates,
+          shipping_address: {
+            company: company,
+            first_name: firstName,
+            last_name: lastName,
+            email: email,
+            address_1: address1,
+            city: city,
+            province: province,
+            postal_code: postalCode,
+            country_code: countryCode,
+            phone: phone,
+            acceptUpdates: acceptUpdates,
           },
           shipping_method: selectedShippingMethod,
         });
 
-        setError('');
         onComplete();
       }
-    } catch (validationError) {
-      if (validationError instanceof ValidationError) {
-        setError('Validation error: ' + validationError.message);
+    } catch (error) {
+      if (error instanceof ValidationError) {
+        setError(new Error('Validation error: ' + error.message));
       } else {
-        setError('An error occurred while updating shipping information.');
+        setError(new Error('An error occurred while updating shipping information.'));
       }
     } finally {
       setIsLoading(false);
     }
   };
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+  const selectedCountryCode = useWatch({ control, name: 'countryCode' });
 
   return (
     <div>
       <h2>Shipping Information</h2>
-      <form onSubmit={handleSubmit(handleFormSubmit)}>
-         <ShippingFormFields
-          control={control as Control<CombinedFormData>}
-           acceptUpdates={acceptUpdates}
-           setAcceptUpdates={setAcceptUpdates}
-           errors={errors}
-           countryOptions={countryOptions}
-         />
-
-        <div>
-          <label htmlFor="acceptUpdates">
+      {isLoading && <div>Loading...</div>}
+      {!isLoading && (
+        <form onSubmit={handleSubmit(handleFormSubmit)}>
+          <Controller
+            name="countryCode"
+            control={control}
+            defaultValue=""
+            render={({ field }) => (
+              <select {...field}>
+                <option value="" disabled>
+                  Select country
+                </option>
+                {countryOptions.map((option) => (
+                  <option key={option.value} value={option.value}>
+                    {option.label}
+                  </option>
+                ))}
+              </select>
+            )}
+          />
+          <Controller
+            name="address1"
+            control={control}
+            defaultValue=""
+            render={({ field }) => <input {...field} placeholder="Address" />}
+          />
+          <Controller
+            name="city"
+            control={control}
+            defaultValue=""
+            render={({ field }) => <input {...field} placeholder="City" />}
+          />
+          <Controller
+            name="postalCode"
+            control={control}
+            defaultValue=""
+            render={({ field }) => <input {...field} placeholder="Postal Code" />}
+          />
+          {selectedCountryCode === 'US' && (
+            <Controller
+              name="province"
+              control={control}
+              defaultValue=""
+              render={({ field }) => <input {...field} placeholder="State" />}
+            />
+          )}
+          <Controller
+            name="phone"
+            control={control}
+            defaultValue=""
+            render={({ field }) => <input {...field} placeholder="Phone" />}
+          />
+          <Controller
+            name="firstName"
+            control={control}
+            defaultValue=""
+            render={({ field }) => <input {...field} placeholder="First Name" />}
+          />
+          <Controller
+            name="lastName"
+            control={control}
+            defaultValue=""
+            render={({ field }) => <input {...field} placeholder="Last Name" />}
+          />
+          <Controller
+            name="email"
+            control={control}
+            defaultValue=""
+            render={({ field }) => <input {...field} placeholder="Email" />}
+          />
+          <Controller
+            name="company"
+            control={control}
+            defaultValue=""
+            render={({ field }) => <input {...field} placeholder="Company" />}
+          />
+          <div>
             <input
               type="checkbox"
               checked={acceptUpdates}
               onChange={() => setAcceptUpdates(!acceptUpdates)}
             />
-            Receive product updates and newsletters
-          </label>
-        </div>
-        {error && <div style={{ color: 'red' }}>{error}</div>}
-        <button type="submit">Save Shipping Address</button>
-      </form>
+            <label htmlFor="acceptUpdates">
+              Receive product updates and newsletters
+            </label>
+          </div>
+          {error && <div style={{ color: 'red' }}>{error.message}</div>}
+          <button type="submit">Save Shipping Address</button>
+        </form>
+      )}
     </div>
   );
 };
-
 
 export default ShippingForm;
