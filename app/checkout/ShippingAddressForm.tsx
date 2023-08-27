bb2f2c77e33be9dfea675d55d0eb8d41f1419062
grammatools1 <121976867+grammatools1@users.noreply.github.com>
2023-08-27 18:40:58 +0200

    Update ShippingAddressForm.tsx

--- app/checkout/ShippingAddressForm.tsx
+++ app/checkout/ShippingAddressForm.tsx
@@ -23,7 +23,7 @@ export interface CombinedFormData {
   countryCode: string;
   postalCode: string;
   phone: string;
-  company?: string;
+  company: string;
 }
 
 const validationSchema = yup.object().shape({
