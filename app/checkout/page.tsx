6a6285994c46138aa5afe047d402303cc6378542
grammatools1 <121976867+grammatools1@users.noreply.github.com>
2023-08-29 20:30:43 +0200

    Update page.tsx

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -7,6 +7,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import { CheckoutFlow } from './checkoutflow';
 
 function Checkout() {
+const [loading, setLoading] = useState(true);
 const CheckoutFlow = dynamic(
   () => import('./checkoutflow').then((mod) => mod.CheckoutFlow),
   { ssr: false }
