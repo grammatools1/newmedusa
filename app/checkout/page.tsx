2ad9311220dbfe5c77ac72d54f18d6777632224a
grammatools1 <121976867+grammatools1@users.noreply.github.com>
2023-08-29 20:41:55 +0200

    Update page.tsx

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -4,14 +4,15 @@ import React, { useState, useEffect, Suspense } from 'react';
 import dynamic from 'next/dynamic';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { CheckoutFlow } from './checkoutflow';
+import CheckoutFlow from './checkoutflow';
 
 function Checkout() {
 const [loading, setLoading] = useState(true);
+  
 const CheckoutFlow = dynamic(
   () => import('./checkoutflow').then((mod) => mod.CheckoutFlow),
   { ssr: false }
-);
+   );
   
   return (
     <>
