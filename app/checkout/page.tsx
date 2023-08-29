8e24bc76bbff1ba78467d09d8d9157481e1e5089
grammatools1 <121976867+grammatools1@users.noreply.github.com>
2023-08-29 19:11:26 +0200

    Update page.tsx

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -3,26 +3,16 @@
 import React, { Suspense } from 'react';
 import dynamic from 'next/dynamic';
 import { useRouter } from 'next/router';
+import Checkoutflow from './Checkoutflow';
 
-
-
-// Assuming Checkoutflow is located in the same directory as Checkout
-const Checkoutflow = dynamic(
-  () => import('./checkoutflow').then((mod) => mod.Checkoutflow),
-  { ssr: false }
-);
-
-function Checkout() {
+const Checkout = () => {
   const router = useRouter();
 
-  // Assuming getCart is a function that retrieves the cart data
   const cart = getCart();
 
   return (
     <>
-      {/* Use Suspense to wrap the Checkoutflow component */}
       <Suspense fallback={<div>Loading...</div>}>
-        {/* Render the dynamic Checkoutflow component */}
         <Checkoutflow cart={cart} />
       </Suspense>
     </>
