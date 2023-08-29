9bae595fce003f371d7a23e84c2db3a808548bd5
grammatools1 <121976867+grammatools1@users.noreply.github.com>
2023-08-29 19:17:18 +0200

    Update page.tsx

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -13,7 +13,7 @@ const Checkout = () => {
   return (
     <>
       <Suspense fallback={<div>Loading...</div>}>
-        <Checkoutflow cart={cart} />
+        <CheckoutFlow cart={cart} />
       </Suspense>
     </>
   );
