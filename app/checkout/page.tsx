0b19e54dface78f508637e7bab2260418aa530fd
grammatools1 <121976867+grammatools1@users.noreply.github.com>
2023-08-29 20:48:51 +0200

    Update page.tsx

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -11,7 +11,6 @@ const medusaBaseUrl = process.env.NEXT_PUBLIC_MEDUSA_BACKEND_API;
 function Checkout() {
 const [loading, setLoading] = useState(true);
 const [cart, setCart] = useState(null);
-const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     async function fetchCart() {
