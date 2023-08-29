4dc7942025e071a377bace5d6ea7899c21b616c0
grammatools1 <121976867+grammatools1@users.noreply.github.com>
2023-08-29 19:18:47 +0200

    Update page.tsx

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -5,6 +5,7 @@ import dynamic from 'next/dynamic';
 import { useRouter } from 'next/router';
 import Checkoutflow from './checkoutflow';
 
+
 const Checkout = () => {
   const router = useRouter();
 
