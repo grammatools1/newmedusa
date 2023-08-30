03d69129152871a200fad20a04879a71d58da8db
grammatools1 <121976867+grammatools1@users.noreply.github.com>
2023-08-30 21:24:05 +0200

    Update page.tsx

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,44 +1,51 @@
 "use client"
 
 import React, { useState, useEffect, Suspense } from 'react';
-import dynamic from 'next/dynamic';
-import { ToastContainer } from 'react-toastify'; 
-import 'react-toastify/dist/ReactToastify.css';
+import { cookies } from 'next/headers';
+import { getCart } from '../lib/medusa';
+import CheckoutFlow from './CheckoutFlow';
+import CartModal from 'components/cart/CartModal';
 
-
-const medusaBaseUrl = process.env.NEXT_PUBLIC_MEDUSA_BACKEND_API;
-
-const CheckoutFlow = dynamic(() => import('./CheckoutFlow'), { ssr: false });
- 
 function Checkout() {
-const [loading, setLoading] = useState(true);
-const [cart, setCart] = useState(null);
- 
+  const [loading, setLoading] = useState(true);
+  const [cart, setCart] = useState();
+
   useEffect(() => {
-    async function fetchCart() {
+    const fetchCart = async () => {
       setLoading(true);
-      const response = await fetch('medusaBaseUrl/store/carts/$cartid'); // Replace with your API endpoint
-      const data = await response.json();
-      setCart(data);
-      setLoading(false); 
-    }
+
+      // Get the cart ID from cookies
+      const cartId = cookies(document.cookie).get('cartId')?.value;
+
+      // If no cart ID is found, set `loading` state to `false`
+      if (!cartId) {
+        setLoading(false);
+        return;
+      }
+
+      try {
+        // Retrieve the cart using the `getCart` function
+        const retrievedCart = await getCart(cartId);
+        setCart(retrievedCart);
+        setLoading(false);
+      } catch (error) {
+        console.log('Error retrieving cart:', error);
+      }
+    };
 
     fetchCart();
   }, []);
-  
+
+  if (loading) {
+    return <div className="loader">Loading...</div>;
+  }
+
   return (
     <>
-      <ToastContainer position="top-right" />
-
-      {loading && <div className="loader">Loading...</div>}
-
-      {!loading && cart && (
-        <div className="checkout-container">
-         <CheckoutFlow cart={cart} />;
-        </div>
-      )}
+      <CartModal cart={cart} />
+      <CheckoutFlow cart={cart} />
     </>
   );
 }
-     
+
 export default Checkout;
