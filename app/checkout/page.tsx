6a8c04c962120dca4c50a76c3f6310e47a8cfc0b
grammatools1 <121976867+grammatools1@users.noreply.github.com>
2023-08-31 20:43:28 +0200

    Update page.tsx

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState, useEffect } from 'react';
 import dynamic from 'next/dynamic';
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer, toast  } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Medusa from '@medusajs/medusa-js';
 import CheckoutFlow from './CheckoutFlow';
