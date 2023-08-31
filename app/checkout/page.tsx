7fa92a8f8ef6c0c85f6cc2bb7b65f37b9ced0f8c
grammatools1 <121976867+grammatools1@users.noreply.github.com>
2023-08-31 20:13:20 +0200

    Update page.tsx

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -4,25 +4,56 @@ import React, { useState, useEffect, Suspense } from 'react';
 import dynamic from 'next/dynamic';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Medusa from '@medusajs/medusa-js';
+import CheckoutFlow from './CheckoutFlow'; 
 
-const CheckoutFlow = dynamic(() => import('./CheckoutFlow'), { ssr: false });
+
 
 function Checkout({ cartId }) {
   const [cart, setCart] = useState(null);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    async function fetchCart() {
-      setLoading(true);
-      const response = await fetch(`/api/cart/${cartId}`); // Use the appropriate API endpoint
-      const data = await response.json();
-      setCart(data);
-      setLoading(false);
-    }
+    const initializeMedusa = async () => {
+      const medusaBaseUrl = process.env.NEXT_PUBLIC_MEDUSA_BACKEND_API;
+      if (!medusaBaseUrl) {
+        console.error('Medusa base URL is not defined.');
+        return;
+      }
 
-    fetchCart();
-  }, [cartId]);
+      const initializedMedusa = new Medusa({
+        baseUrl: medusaBaseUrl,
+        maxRetries: 3,
+      });
+      setMedusa(initializedMedusa);
+    };
 
+    initializeMedusa();
+  }, []);
+
+ 
+  useEffect(() => {
+    fetchCartItems(cart);
+  }, [cart]);
+
+  const fetchCartItems = async (cart: { id: string }) => {
+  if (!medusa) {
+    console.error('Medusa not initialized');
+    return;
+  }
+
+  try {
+    setLoading(true);
+    const { cart: updatedCart } = await medusa.carts.retrieve(cart.id);
+    setOrderTotal(updatedCart.total);
+    setCartItems(updatedCart.items);
+  } catch (error) {
+    console.error('Error fetching cart items:', error);
+    toast.error('Failed to fetch cart items. Please refresh the page.', { autoClose: 3000 });
+  } finally {
+    setLoading(false);
+  }
+};
   return (
     <>
       <ToastContainer position="top-right" />
