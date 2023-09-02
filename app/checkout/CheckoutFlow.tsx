ea0d28bc90a2a17668b071d2af5afc8625b481a5
grammatools1 <121976867+grammatools1@users.noreply.github.com>
2023-09-02 19:43:11 +0200

    Update CheckoutFlow.tsx

--- app/checkout/CheckoutFlow.tsx
+++ app/checkout/CheckoutFlow.tsx
@@ -275,7 +275,8 @@ type Props = {
                     </div>
                   </div>
                   <p className="order-total">Order Total: ${orderTotal.toFixed(2)}</p>
-                  <ShippingForm cartId={cartId} onComplete={handleShippingComplete} />
+                  <ShippingForm cartId={cartId || undefined} onComplete={handleShippingComplete} />
+
                 </>
               )}
             </div>
