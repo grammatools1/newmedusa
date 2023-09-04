0562fbb83992b439756f14170d00b47ab688c38a
grammatools1 <121976867+grammatools1@users.noreply.github.com>
2023-09-04 20:22:07 +0200

    Create getCartId.tsx

---
+++ app/checkout/getCartId.tsx
@@ -0,0 +1,12 @@
+import { cookies } from 'next/headers';
+import CheckoutFlow from './CheckoutFlow';
+
+export default async function GetcartId() {
+  const cartId = cookies().get('cartId')?.value;
+  console.log(cartId);
+  return (
+    <>
+      <CheckoutFlow cartId={cartId} />
+    </>
+  );
+}
