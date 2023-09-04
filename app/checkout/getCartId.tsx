4b3fae523432e97735d4f57fe001ebb66e5cb121
grammatools1 <121976867+grammatools1@users.noreply.github.com>
2023-09-04 20:51:36 +0200

    Update getCartId.tsx

--- app/checkout/getCartId.tsx
+++ app/checkout/getCartId.tsx
@@ -1,19 +1,19 @@
+import React from 'react';
 import { cookies } from 'next/headers';
 import CheckoutFlow from './CheckoutFlow';
 
-export default async function GetcartId() {
+export default function GetcartId() {
   const cartId = cookies().get('cartId')?.value;
-  console.log(cartId);
+
   return (
-    <>
-    <CheckoutFlow cartId={cartId || ''}
-  onComplete={() => {
-   alert("Checkout completed! Thank you for your order."); 
-  }}
-  onCartUpdate={(cart) => {
-     console.log("Cart updated:", cart);
-  }}
-/>
-    </>
+    <CheckoutFlow
+      cartId={cartId || ''}
+      onComplete={() => {
+        alert("Checkout completed! Thank you for your order.");
+      }}
+      onCartUpdate={(cart) => {
+        console.log("Cart updated:", cart);
+      }}
+    />
   );
 }
