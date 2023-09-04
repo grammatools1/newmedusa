e04cad3ac71e2f55b59db6f278c6f97947e4d16a
grammatools1 <121976867+grammatools1@users.noreply.github.com>
2023-09-04 21:50:41 +0200

    Update CheckoutFlow.tsx

--- app/checkout/CheckoutFlow.tsx
+++ app/checkout/CheckoutFlow.tsx
@@ -1,50 +1,28 @@
 "use client"
 
-import React, { useState, useEffect, useCallback, useMemo } from 'react';
+import React, { useState, useEffect } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Medusa from '@medusajs/medusa-js';
 import ShippingForm from './ShippingForm';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';;
-
-const PaymentMethod = {
-  credit_card: {
-    card_number: '',
-    exp_month: '',
-    exp_year: '',
-    cvv: '',
-  },
-  paypal: {
-    email: '',
-  },
-  crypto: {
-    wallet_address: '',
-  },
-};
-type PaymentMethodKey = keyof typeof PaymentMethod;
 
 type Props = {
   cartId: string;
-  onComplete: () => void;
   onCartUpdate: (cart: { id: string }) => void;
-}
+  onComplete: () => void;
+};
 
-function CheckoutFlow({ cartId, onComplete, onCartUpdate }: Props) {
-  const [medusa, setMedusa] = useState<Medusa | null>(null);
+function CheckoutFlow({ cartId, onCartUpdate, onComplete }: Props) {
+  const [shippingAddress, setShippingAddress] = useState<any>({});
   const [loading, setLoading] = useState(false);
-  const [selectedPaymentMethod, setSelectedPaymentMethod] = useState<keyof typeof PaymentMethod>("credit_card");
-  const [couponCode, setCouponCode] = useState('');
-  const [giftCardCode, setGiftCardCode] = useState('');
-    const [orderTotal, setOrderTotal] = useState<number | null>(null);
-  const [cartItems, setCartItems] = useState<any[]>([]);
-  const [step, setStep] = useState(1);
-  const [confirmOrder, setConfirmOrder] = useState(false);
- 
-  
-  const medusaBaseUrl = process.env.NEXT_PUBLIC_MEDUSA_BACKEND_API;
-  
+  const [shippingMethods, setShippingMethods] = useState<any[]>([]);
+  const [medusa, setMedusa] = useState<Medusa | null>(null);
+  const [orderTotal, setOrderTotal] = useState<number | null>(null);
+
   useEffect(() => {
     const initializeMedusa = async () => {
-     
+      const medusaBaseUrl = process.env.NEXT_PUBLIC_MEDUSA_BACKEND_API;
+
       if (!medusaBaseUrl) {
         console.error('Medusa base URL is not defined.');
         return;
@@ -66,14 +44,14 @@ function CheckoutFlow({ cartId, onComplete, onCartUpdate }: Props) {
   }, []);
 
   useEffect(() => {
-    if (cartId) {
-      fetchCartItems(cartId);
+    if (cartId && medusa) {
+      fetchShippingProfile(cartId);
     }
   }, [cartId, medusa]);
 
-  const fetchCartItems = async (cartId: string) => {
-    console.log('cartId:', cartId);
-  
+  const fetchShippingProfile = async (cartId: string) => {
+    console.log('fetching shipping profile for cartId:', cartId);
+
     if (!medusa) {
       console.error('Medusa not initialized');
       return;
@@ -81,140 +59,71 @@ function CheckoutFlow({ cartId, onComplete, onCartUpdate }: Props) {
 
     try {
       setLoading(true);
-      const { cart: updatedCart } = await medusa.carts.retrieve(cartId);
-    
-      if (!updatedCart) {
+      const { cart } = await medusa.carts.retrieve(cartId);
+
+      if (!cart) {
         console.error('Cart data is undefined or null');
         return;
       }
 
-      setOrderTotal(updatedCart.total);
-      console.log('orderTotal:', updatedCart.total);
-      setCartItems(updatedCart.items);
-      console.log('cartItems:', updatedCart.items);
-      onCartUpdate({ id: cartId }); // Update cart state in parent component using onCartUpdate prop
+      setOrderTotal(cart.total);
+      console.log('orderTotal:', cart.total);
+
+      const { profile_id: profileId } = cart;
+      const { shipping_profile } = await medusa.cartShippingProfiles.retrieve({
+        cart_id: cartId,
+        profile_id: profileId,
+      });
+
+      if (!shipping_profile) {
+        console.error('Shipping profile data is undefined or null');
+        return;
+      }
+
+      setShippingMethods(shipping_profile.shipping_methods);
+      console.log('shippingMethods:', shipping_profile.shipping_methods);
     } catch (error) {
-      console.error('Error fetching cart items:', error);
-      toast.error('Failed to fetch cart items. Please refresh the page.', { autoClose: 3000 });
+      console.error('Error fetching shipping profile:', error);
+      toast.error('Failed to fetch shipping profile. Please refresh the page.', { autoClose: 3000 });
     } finally {
       setLoading(false);
     }
   };
 
-  const handleShippingComplete = () => {
-    setStep(2);
-  };
+  const handleShippingComplete = async () => {
+    console.log('handleShippingComplete()');
 
-  const handlePaymentComplete = () => {
-    setStep(3);
-  };
-
-  const handlePlaceOrder = async () => {
-    if (!medusa || !cartId) return;
+    if (!medusa || !cartId) {
+      console.error('Medusa not initialized or cart ID not present');
+      return;
+    }
 
     try {
       setLoading(true);
-      const paymentData = {
-        provider_id: selectedPaymentMethod,
-        data: {
-          ...PaymentMethod[selectedPaymentMethod],
-        },
-      };
+      const { cart } = await medusa.carts.retrieve(cartId);
 
-      await medusa.carts.setPaymentSession(cartId, paymentData);
-      const { type, data } = await medusa.carts.complete(cartId);
-      console.log('Checkout Completed:', type, data);
-      toast.success('Your order has been successfully placed!', { autoClose: 3000 });
-      setConfirmOrder(false);
-      // TODO: Display order confirmation or handle any further actions
+      if (!cart) {
+        console.error('Cart data is undefined or null');
+        return;
+      }
+
+      const { profile_id: profileId } = cart;
+      const result = await medusa.checkout.complete({ cart_id: cartId, profile_id: profileId });
+
+      if (result.status === 'ok') {
+        console.log('checkout completed successfully!');
+        onComplete();
+      } else {
+        console.error('checkout failed:', result.message);
+        toast.error(`Failed to complete checkout: ${result.message}`, { autoClose: 3000 });
+      }
     } catch (error) {
       console.error('Error completing checkout:', error);
-      toast.error('Failed to place order. Please try again or contact support.', { autoClose: 3000 });
+      toast.error('Failed to complete checkout. Please refresh the page.', { autoClose: 3000 });
     } finally {
       setLoading(false);
     }
   };
-   
-  const handleApplyCoupon = async () => {
-    if (!medusa || !cartId || !couponCode) return;
-
-    try {
-      const { cart: updatedCartData } = await medusa.carts.update(cartId, {
-        discounts: [{ code: couponCode }],
-      });
-      setOrderTotal(updatedCartData.total);
-      setCouponCode("");
-
-      toast.success("Coupon applied!", { autoClose: 3000 });
-    } catch (error) {
-      console.error("Error applying coupon:", error);
-      toast.error("Failed to apply coupon. Please try again or contact support.", { autoClose: 3000 });
-    }
-  };
-
-  const handleApplyGiftCard = useCallback(async () => {
-    if (!medusa || !cartId || !giftCardCode) return;
-
-   try {
-      const { cart: updatedCart } = await medusa.carts.update(cartId, {
-        gift_cards: [{ code: giftCardCode }],
-      });
-      setOrderTotal(updatedCart.total);
-
-      toast.success("Gift card applied!", { autoClose: 3000 });
-    } catch (error) {
-      console.error("Error applying gift card:", error);
-      toast.error("Failed to apply gift card. Please try again or contact support.", { autoClose: 3000 });
-    } 
-  }, [cartId, giftCardCode, medusa]);
-
-  const validateForm = (formValues: any) => {
-    const errors: any = {};
-
-    // Add validation checks here for each form value
-
-    return errors;
-  };
-
-  const handleGoBack = () => {
-    setStep(step - 1);
-  };
-
-  const handleNextStep = useCallback(() => {
-    const errors = validateForm({});
-
-    if (Object.keys(errors).length === 0) {
-      setStep(step + 1);
-    } else {
-      // Show form errors to the user
-    }
-  }, [step, validateForm]);
-
-  const handleConfirmOrder = () => {
-    setConfirmOrder(true);
-  };
-
-  const handleCancelOrder = () => {
-    setConfirmOrder(false);
-  };
-
-  const handleCreditCardChange = useCallback((e: React.FormEvent<HTMLInputElement>) => {
-    const { name, value } = e.currentTarget;
-    setSelectedPaymentMethod('credit_card');
-  }, []);
-
-  const handlePaypalChange = useCallback((e: React.FormEvent<HTMLInputElement>) => {
-    const { name, value } = e.currentTarget;
-    setSelectedPaymentMethod('paypal');
-  }, []);
-
-  const handleCryptoChange = useCallback((e: React.FormEvent<HTMLInputElement>) => {
-    const { name, value } = e.currentTarget;
-    setSelectedPaymentMethod('crypto');
-  }, []);
-
-  const memoizedHandleApplyCoupon = useMemo(() => handleApplyCoupon, [handleApplyCoupon]);
-  const memoizedHandleApplyGiftCard = useMemo(() => handleApplyGiftCard, [handleApplyGiftCard]);
 
   return (
     <>
@@ -222,125 +131,35 @@ function CheckoutFlow({ cartId, onComplete, onCartUpdate }: Props) {
 
       {loading && <div className="loader">Loading...</div>}
 
-      {!loading && (
-        <div className="checkout-flow">
-          {step === 1 && (
+      {!loading && shippingMethods && (
+        <>
+          <div className="shipping-methods">
+            <h2>Shipping Methods</h2>
             <div>
-              {/* Step 1: Cart Review */}
-              <h1>Step 1: Cart Review</h1>
-              {cartItems.length === 0 ? (
-                <p>Your cart is empty.</p>
-              ) : (
-                <>
-                  <ul>
-                   {cartItems.map((item: any) => (
-                      <li key={item.id}>
-                        {item.quantity} x {item.product ? item.product.title : 'Product Title Not Available'} - ${item.total.toFixed(2)}
-                      </li>
-                    ))}
-                  </ul>
-                  <div className="coupon-gift">
-                    <div>
-                      <label htmlFor="coupon">Coupon Code:</label>
-                      <input
-                        id="coupon"
-                        type="text"
-                        placeholder="Enter coupon code"
-                        value={couponCode}
-                        onChange={(e) => setCouponCode(e.target.value)}
-                      />
-                      <button onClick={memoizedHandleApplyCoupon}>Apply Coupon</button>
-                    </div>
-                    <div>
-                      <label htmlFor="gift-card">Gift Card Code:</label>
-                      <input
-                        id="gift-card"
-                        type="text"
-                        placeholder="Enter gift card code"
-                        value={giftCardCode}
-                        onChange={(e) => setGiftCardCode(e.target.value)}
-                      />
-                      <button onClick={memoizedHandleApplyGiftCard}>Apply Gift Card</button>
-                    </div>
-                  </div>
-                  <p className="order-total">Order Total: ${orderTotal.toFixed(2)}</p>
-                  <ShippingForm cartId={cartId} onComplete={handleShippingComplete} onCartUpdate={onCartUpdate} /> {/* pass onCartUpdate prop to ShippingForm */}
-                </>
-              )}
+              {shippingMethods.map((method) => (
+                <div key={method.id}>
+                  <label>
+                    <input
+                      type="radio"
+                      name="shippingMethod"
+                      value={method.id}
+                      onChange={() => console.log('selected method:', method)}
+                    />
+                    {method.name}
+                    <span>{`$${method.price.toFixed(2)}`}</span>
+                  </label>
+                </div>
+              ))}
             </div>
-          )}
-          {step === 2 && (
-            <div>
-              {/* Step 2: Shipping Information */}
-              <h1>Step 2: Shipping Information</h1>
-              <button onClick={handleGoBack}>Go back</button>
-              <ShippingForm cartId={cartId} onComplete={handlePaymentComplete} onCartUpdate={onCartUpdate} /> {/* pass onCartUpdate prop to ShippingForm */}
-            </div>
-          )}
-          {step === 3 && (
-            <div>
-              {/* Step 3: Payment Method Selection */}
-              <h1>Step 3: Payment Method Selection</h1>
-              <button onClick={handleGoBack}>Go back</button>
-              <div className="payment-methods">
-                <label>
-                  <input
-                    type="radio"
-                    name="paymentMethod"
-                    value="credit_card"
-                    checked={selectedPaymentMethod === 'credit_card'}
-                    onChange={handleCreditCardChange}
-                  />
-                  Credit Card
-                </label>
-                <label>
-                  <input
-                    type="radio"
-                    name="paymentMethod"
-                    value="paypal"
-                    checked={selectedPaymentMethod === 'paypal'}
-                    onChange={handlePaypalChange}
-                  />
-                  PayPal
-                </label>
-                <label>
-                  <input
-                    type="radio"
-                    name="paymentMethod"
-                    value="crypto"
-                    checked={selectedPaymentMethod === 'crypto'}
-                    onChange={handleCryptoChange}
-                  />
-                  Crypto
-                </label>
-              </div>
-              <button onClick={handlePaymentComplete}>Proceed to Order Review</button>
-            </div>
-          )}
-          {step === 4 && (
-            <div>
-              {/* Step 4: Order Review */}
-              <h1>Step 4: Order Review</h1>
-              <button onClick={handleGoBack}>Go back</button>
-              <div className="order-summary">
-                {/* Display order summary */}
-                <p>Selected Payment Method: {selectedPaymentMethod}</p>
-                {/* Display other order details like shipping method, total price, etc. */}
-              </div>
-              <div>
-                {confirmOrder ? (
-                  <>
-                    <p>Please review your order and click "Place Order" to confirm.</p>
-                    <button onClick={handleCancelOrder}>Cancel</button>
-                    <button onClick={handlePlaceOrder}>Place Order</button>
-                  </>
-                ) : (
-                  <button onClick={handleConfirmOrder}>Confirm Order</button>
-                )}
-              </div>
-            </div>
-          )}
-        </div>
+          </div>
+          <div className="checkout-actions">
+            {orderTotal !== null && (
+              <p className="order-total">Order Total: ${orderTotal.toFixed(2)}</p>
+            )}
+            <ShippingForm cartId={cartId} onComplete={handleShippingComplete} onCartUpdate={onCartUpdate} />
+            {/* pass onCartUpdate prop to ShippingForm */}
+          </div>
+        </>
       )}
     </>
   );
