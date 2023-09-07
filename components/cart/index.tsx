cd7154d03bf4b401ba256d3fe93576ace91b9add
grammatools1 <121976867+grammatools1@users.noreply.github.com>
2023-09-07 20:35:48 +0200

    Update index.tsx

--- components/cart/index.tsx
+++ components/cart/index.tsx
@@ -30,7 +30,7 @@ export default async function Cart() {
         console.log("Cart updated:", cart);
       }}
     />
-      <CartModal cart={cart} />
+      <CartModal cart={cart} /> 
     </>
   );
 }
