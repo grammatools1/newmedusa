da90550ae38fee100e462a8f6442ba7daf646ef1
grammatools1 <121976867+grammatools1@users.noreply.github.com>
2023-09-08 16:35:59 +0200

    Update index.tsx

--- components/cart/index.tsx
+++ components/cart/index.tsx
@@ -7,29 +7,29 @@ export default async function Cart() {
   const cartId = cookies().get('cartId')?.value;
    console.log(cartId);
   
-  let cart: CartType | undefined;
-
   if (cartId) {
     cart = await getCart(cartId);
   }
 
   // If the `cartId` from the cookie is not set or the cart is empty
-  // (old carts becomes `null` when you checkout), then get a new `cartId`
+  // (old carts become `null` when you checkout), then get a new `cartId`
   //  and re-fetch the cart.
   if (!cartId || !cart) {
     cart = await createCart();
   }
-   onComplete=() => {
-        alert("Checkout completed! Thank you for your order.");
-      };
-  onCartUpdate=() => {
-        console.log("Cart updated:", cart);
-      };
-  
+
+  const onComplete = () => {
+    alert("Checkout completed! Thank you for your order.");
+  };
+
+  const onCartUpdate = () => {
+    console.log("Cart updated:", cart);
+  };
+
   return (
     <>
-      <CheckoutFlow cart={cart} onComplete={cart} onCartUpdate={cart}/>
-      <CartModal cart={cart} /> 
+      <CheckoutFlow cart={cart} onComplete={onComplete} onCartUpdate={onCartUpdate} /> 
+      <CartModal cart={cart} />
     </>
   );
 }
