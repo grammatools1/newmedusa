aacbdee0415b45f85e4b0c37bd2514e2d5a5c5d0
grammatools1 <121976867+grammatools1@users.noreply.github.com>
2023-09-23 08:37:06 +0200

    Update index.tsx

--- components/checkout/components/checkout-loader/index.tsx
+++ components/checkout/components/checkout-loader/index.tsx
@@ -2,7 +2,7 @@
 
 import { Dialog, Transition } from "@headlessui/react"
 import { useCheckout } from "components/context/checkout-context"
-import noop from "components/noop"
+import noop from "components/util/noop"
 import Spinner from "components/checkout/common/icons/spinner"
 
 const CheckoutLoader = () => {
